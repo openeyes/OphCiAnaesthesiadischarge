b89009a266503363becdbaca82a4945c77990254
adamdyer <progen91@gmail.com>
2014-04-24 16:15:52 +0100

    [ORB-23] elements

---
+++ migrations/m140424_123127_event_type_OphCiAnaesthesiadischarge.php
@@ -0,0 +1,73 @@
+<?php 
+class m140424_123127_event_type_OphCiAnaesthesiadischarge extends CDbMigration
+{
+	public function up()
+	{
+		if (!$this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphCiAnaesthesiadischarge'))->queryRow()) {
+			$group = $this->dbConnection->createCommand()->select('id')->from('event_group')->where('name=:name',array(':name'=>'Clinical events'))->queryRow();
+			$this->insert('event_type', array('class_name' => 'OphCiAnaesthesiadischarge', 'name' => 'AnaesthesiaDischarge','event_group_id' => $group['id']));
+		}
+
+		$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphCiAnaesthesiadischarge'))->queryRow();
+
+		if (!$this->dbConnection->createCommand()->select('id')->from('element_type')->where('name=:name and event_type_id=:eventTypeId', array(':name'=>'Patient Discharge Criteria',':eventTypeId'=>$event_type['id']))->queryRow()) {
+			$this->insert('element_type', array('name' => 'Patient Discharge Criteria','class_name' => 'Element_OphCiAnaesthesiadischarge_PatientDischargeCriteria', 'event_type_id' => $event_type['id'], 'display_order' => 1));
+		}
+
+		$element_type = $this->dbConnection->createCommand()->select('id')->from('element_type')->where('event_type_id=:eventTypeId and name=:name', array(':eventTypeId'=>$event_type['id'],':name'=>'Patient Discharge Criteria'))->queryRow();
+
+
+
+		$this->createTable('et_ophcianaesthesiadischarge_patientdischarge', array(
+				'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
+				'event_id' => 'int(10) unsigned NOT NULL',
+				'pain_score_1_or_less' => 'tinyint(1) unsigned NOT NULL',
+
+				'apvu' => 'tinyint(1) unsigned NOT NULL',
+
+				'mews_score_1_or_less' => 'tinyint(1) unsigned NOT NULL',
+
+				'nausea_vomiting_score_1_or_less' => 'tinyint(1) unsigned NOT NULL',
+
+				'blood_loss_score_1_or_less' => 'tinyint(1) unsigned NOT NULL',
+
+				'diabetics_blood_sugar_range' => 'tinyint(1) unsigned NOT NULL',
+
+				'anaesthesia_summary' => 'text COLLATE utf8_bin DEFAULT \'\'',
+
+				'patient_reviewed_and_discharged' => 'tinyint(1) unsigned NOT NULL',
+
+				'last_modified_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+				'last_modified_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+				'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT 1',
+				'created_date' => 'datetime NOT NULL DEFAULT \'1901-01-01 00:00:00\'',
+				'PRIMARY KEY (`id`)',
+				'KEY `et_ophcianaesthesiadischarge_patientdischarge_lmui_fk` (`last_modified_user_id`)',
+				'KEY `et_ophcianaesthesiadischarge_patientdischarge_cui_fk` (`created_user_id`)',
+				'KEY `et_ophcianaesthesiadischarge_patientdischarge_ev_fk` (`event_id`)',
+				'CONSTRAINT `et_ophcianaesthesiadischarge_patientdischarge_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `et_ophcianaesthesiadischarge_patientdischarge_cui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `et_ophcianaesthesiadischarge_patientdischarge_ev_fk` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`)',
+			), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin');
+
+	}
+
+	public function down()
+	{
+		$this->dropTable('et_ophcianaesthesiadischarge_patientdischarge');
+
+
+
+
+		$event_type = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name=:class_name', array(':class_name'=>'OphCiAnaesthesiadischarge'))->queryRow();
+
+		foreach ($this->dbConnection->createCommand()->select('id')->from('event')->where('event_type_id=:event_type_id', array(':event_type_id'=>$event_type['id']))->queryAll() as $row) {
+			$this->delete('audit', 'event_id='.$row['id']);
+			$this->delete('event', 'id='.$row['id']);
+		}
+
+		$this->delete('element_type', 'event_type_id='.$event_type['id']);
+		$this->delete('event_type', 'id='.$event_type['id']);
+	}
+}
+
---
+++ views/default/form_Element_OphCiAnaesthesiadischarge_PatientDischargeCriteria.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+?>
+
+<section class="element <?php echo $element->elementType->class_name?>"
+				 data-element-type-id="<?php echo $element->elementType->id?>"
+				 data-element-type-class="<?php echo $element->elementType->class_name?>"
+				 data-element-type-name="<?php echo $element->elementType->name?>"
+				 data-element-display-order="<?php echo $element->elementType->display_order?>">
+	<header class="element-header">
+		<h3 class="element-title"><?php echo $element->elementType->name; ?></h3>
+	</header>
+
+	<div class="element-fields">
+		<div class="element-data element-eyes row">
+			<div class="element-eye right-eye column">
+				<div class="data-row">
+					<?php echo $form->radioBoolean($element, 'pain_score_1_or_less')?>
+					<?php echo $form->radioBoolean($element, 'apvu')?>
+					<?php echo $form->radioBoolean($element, 'mews_score_1_or_less')?>
+					<?php echo $form->radioBoolean($element, 'nausea_vomiting_score_1_or_less')?>
+				</div>
+			</div>
+			<div class="element-eye left-eye column">
+				<div class="data-row">
+					<?php echo $form->radioBoolean($element, 'blood_loss_score_1_or_less')?>
+					<?php echo $form->radioBoolean($element, 'diabetics_blood_sugar_range')?>
+				</div>
+			</div>
+		</div>
+		<BR/>
+		<div align="center"><div class="wrapper field-highlight inline" style="background-color: limegreen; padding:10px; border: 1px solid black; display:none">Discharge Criteria Met</div></div>
+		<div class="element-fields">
+			<?php	$form->layoutColumns = array('label' => 2,'field' => 10);	?>
+			<?php echo $form->textArea($element, 'anaesthesia_summary', array('rows' => 6, 'cols' => 80))?>
+			<?php echo $form->checkBox($element, 'patient_reviewed_and_discharged')?>
+		</div>
+	</div>
+	<?php	$form->layoutColumns = array('label' => 4,'field' => 8);	?>
+
+</section>
